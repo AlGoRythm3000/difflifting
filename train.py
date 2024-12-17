cac7f47eb0b7aebb84a05e37e0b4696cd9e22ade
Gabriel Duarte <gabrieljduarte98@gmail.com>
2024-12-17 10:19:29 -0300

    add tqdm

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 def train(loader, model, loss_fn, optimizer, device):
     model.train()
     train_losses = []
-    for batch in loader:
+    for batch in tqdm(loader):
         batch = batch.to(device)
         optimizer.zero_grad()
         out = model(batch)
@@ -19,7 +19,7 @@ def train(loader, model, loss_fn, optimizer, device):
 @torch.no_grad()
 def evaluate(model, loader, loss_fn, device, evaluator=None):
     model.eval()
-    for batch in loader:
+    for batch in tqdm(loader):
         batch = batch.to(device)
         out = model(batch)
         loss = loss_fn(out.squeeze().float(), batch.y.squeeze())
