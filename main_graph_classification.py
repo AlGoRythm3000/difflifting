fixed problems, corrected validation calculation

main_graph_classification.py
@@ -22,14 +22,12 @@ triangle_counts = []  # Add this list to store triangle counts
 
 torch.autograd.set_detect_anomaly(True)
 import tempfile
-import mlflow.pytorch
 if __name__ == '__main__':
 
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     args = parse_args()
-    mlflow.set_experiment(f"/{args.tnn}_{args.lifting}_{args.seed}_{args.dataset}")
     set_seed(args.seed)
     print(args.__dict__)
     data, num_features, num_classes = choose_dataset(args, device)
@@ -46,6 +44,7 @@ if __name__ == '__main__':
 
 
 
+
     def train_eval(model, train_loader, val_loader, test_loader, loss_fn, optimizer, evaluator, device):
         train_loss = train(train_loader, model, loss_fn, optimizer, device)
         val_loss, val_acc = evaluate(model, val_loader, loss_fn, device, evaluator)
@@ -58,7 +57,7 @@ if __name__ == '__main__':
     test_accuracies = []
     val_losses = []
     val_accuracies = []
-
+    epochs_no_improve=0
     print(
         "Number of parameters:",
         sum(p.numel() for p in model.parameters() if p.requires_grad),
@@ -80,60 +79,43 @@ if __name__ == '__main__':
 
 
 
-    with mlflow.start_run() as run:
+    for epoch in range(1, args.max_epochs):
+        train_loss, val_loss, val_acc, test_loss, test_acc = train_eval(
+            model,
+            train_loader,
+            val_loader,
+            test_loader,
+            loss_fn,
+            optimizer,
+            evaluator,
+            device
+        )
 
-        mlflow.log_params(args.__dict__,)
+        test_accuracies.append(test_acc)
+        test_losses.append(test_loss)  # test losses
 
+        val_accuracies.append(val_acc)
+        val_losses.append(val_loss)  # test losses
 
-        mlflow.pytorch.log_model(model, 'models')
-        with open("model_summary.txt", "w", encoding="utf-8") as f:
-            f.write(str(summary(model, depth=5, verbose=2)))
-        mlflow.log_artifact("model_summary.txt")
-        for epoch in range(1, args.max_epochs):
-            train_loss, val_loss, val_acc, test_loss, test_acc = train_eval(
-                model,
-                train_loader,
-                val_loader,
-                test_loader,
-                loss_fn,
-                optimizer,
-                evaluator,
-                device
-            )
-            mlflow.log_metric('train loss',torch.tensor(train_loss).mean().item(), step=epoch)
-            mlflow.log_metric('val loss', val_loss.item(), step=epoch)
-            mlflow.log_metric('test loss', test_loss.item(), step=epoch)
-            mlflow.log_metric('test acc', test_acc.item(), step=epoch)
-            mlflow.log_metric('val acc', test_acc.item(), step=epoch)
-            mlflow.pytorch.autolog()
-            # for name, param in model.named_parameters():
-            #     print(f"{name} gradient: {param.grad}")
-            test_accuracies.append(test_acc)
-            test_losses.append(test_loss)  # test losses
+        train_losses.append(train_loss)  # train losses
 
-            val_accuracies.append(val_acc)
-            val_losses.append(val_loss)  # test losses
+        print(
+            f"{epoch:3d}: Train Loss: {train_loss:.3f},"
+            f" Val Loss: {val_loss:.3f}, Val Acc: {val_accuracies[-1]:.3f}, "
+            f"Test Loss: {test_loss:.3f}, Test Acc: {test_accuracies[-1]:.3f}"
+        )
 
-            train_losses.append(train_loss)  # train losses
+        scheduler.step(val_acc)
 
-            # if (epoch - 1) % args.interval == 0:
-            print(
-                f"{epoch:3d}: Train Loss: {train_loss:.3f},"
-                f" Val Loss: {val_loss:.3f}, Val Acc: {val_accuracies[-1]:.3f}, "
-                f"Test Loss: {test_loss:.3f}, Test Acc: {test_accuracies[-1]:.3f}"
-            )
+        if epoch > 2 and val_accuracies[-1] <= val_accuracies[-2 - epochs_no_improve]:
+            epochs_no_improve = epochs_no_improve + 1
 
-            scheduler.step(val_acc)
+        else:
+            epochs_no_improve = 0
 
-            if epoch > 2 and val_accuracies[-1] <= val_accuracies[-2 - epochs_no_improve]:
-                epochs_no_improve = epochs_no_improve + 1
-
-            else:
-                epochs_no_improve = 0
-
-            if epochs_no_improve >= args.early_stop_patience:
-                print("Early stopping!")
-                break
+        if epochs_no_improve >= args.early_stop_patience:
+            print("Early stopping!")
+            break
 
     results = {
         "train_losses": tensor(train_losses),

train.py
@@ -4,37 +4,30 @@ from tqdm import tqdm
 
 def train(loader, model, loss_fn, optimizer, device):
     model.train()
-    train_losses = []
+    total_loss = 0
     for batch in loader:
         batch = batch.to(device)
         optimizer.zero_grad()
         out = model(batch)
-        loss = loss_fn(out.squeeze().float(), batch.y.squeeze())/len(batch)#.mean(dim=0)
+        loss = loss_fn(out.squeeze(), batch.y.squeeze()) / batch.num_graphs
         loss.backward()
         optimizer.step()
-        train_losses.append(loss*len(batch))
-    return torch.stack(train_losses).mean() 
+        total_loss += loss.item()
+    return total_loss / len(loader)
 
 @torch.no_grad()
 def evaluate(model, loader, loss_fn, device, evaluator=None):
     model.eval()
-    loss = 0
-    acc = 0
+    total_loss = 0
+    total_correct = 0
     for batch in loader:
         batch = batch.to(device)
         out = model(batch)
-        loss += loss_fn(out.squeeze().float(), batch.y.squeeze())
-        acc = -loss
+        loss = loss_fn(out.squeeze(), batch.y.squeeze()) / batch.num_graphs
+        total_loss += loss.item()
         if not isinstance(loss_fn, torch.nn.L1Loss):
-            acc = (out.argmax(dim=-1) == batch.y.squeeze()).float().sum()
-
-    acc/=loader.dataset.data.y.shape[0]
+            total_correct += (out.argmax(dim=-1) == batch.y.squeeze()).sum().item()
+    accuracy = total_correct / loader.dataset.len()
     if evaluator is not None:
-        acc = evaluator.eval({"y_pred": out[:, 1].unsqueeze(dim=1), "y_true": batch.y})[
-            evaluator.eval_metric
-        ]
-    return loss/len(loader), acc
-
-
-
-
+        accuracy = evaluator.eval({"y_pred": out[:, 1].unsqueeze(-1), "y_true": batch.y})[evaluator.eval_metric]
+    return total_loss / len(loader), accuracy