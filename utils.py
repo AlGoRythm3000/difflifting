96c7ff01da8ea20625dbac081e008c3b83d44dd4
JorgeLuizFranco <jorgelwyz.franco@gmail.com>
2025-01-06 18:57:45 -0300

    changed name to AST

--- utils.py
+++ utils.py
@@ -51,7 +51,7 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="DiffLifting for GNN tasks")
     parser.add_argument("--seed", type=int, default=42, help="Random seed.")
     parser.add_argument("--gnn", type=str, default="GPS", choices=["GIN", "GPS"])
-    parser.add_argument("--tnn", type=str, default="AllsetTransformer", choices=["CWN", "SCN2", "CXN", "UniGCNII", "AllsetTransformer"])
+    parser.add_argument("--tnn", type=str, default="AST", choices=["CWN", "SCN2", "CXN", "UniGCNII", "AST"])
     parser.add_argument(
         "--dataset",
         type=str,
