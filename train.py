9ae452cffa614aeb33abefa0811f078ff6fc27d8
Gabriel Duarte <gabrieljduarte98@gmail.com>
2025-03-07 13:15:25 -0300

    Remove graphviz

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import torch
-from torchviz import make_dot
 from tqdm import tqdm
 
 
@@ -10,16 +9,8 @@ def train(loader, model, loss_fn, optimizer, device):
         batch = batch.to(device)
         optimizer.zero_grad()
         out = model(batch)
-        make_dot(out, params=dict(model.named_parameters()), show_attrs=True).render('nn3', format="svg")
         loss = loss_fn(out.squeeze(), batch.y.squeeze()) / batch.num_graphs
         loss.backward()
-
-
-        for name, param in model.named_parameters():
-            if param.grad is None:
-                print(f"No gradient for {name}")
-            elif param.grad.abs().mean() < 1e-10:
-                print(f"Near-zero gradient for {name}")
         optimizer.step()
         total_loss += loss.item()
     return total_loss / len(loader)
