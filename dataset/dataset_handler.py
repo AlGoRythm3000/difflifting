81acca6bf98d2db1f23eeca3f37ddbd11132340a
JorgeLuizFranco <jorgelwyz.franco@gmail.com>
2025-05-04 20:00:40 -0300

    added discrete lifting

--- dataset/dataset_handler.py
+++ dataset/dataset_handler.py
@@ -21,6 +21,7 @@ from tools.lifting.khop import SimplicialKHopLifting
 from tools.lifting.hypergraph import HypergraphKHopLifting
 from tools.lifting.neighboorhood_complex import NeighborhoodComplexLifting
 from tools.lifting.cycle_lifting import CellCycleLifting
+from tools.lifting.discrete_lifting import DiscreteConfigurationComplexLifting
 from tools.normalize import normalize_matrix
 
 NODES_PREDICTION_DATASET = ["Cora", "Citeseer", "Pubmed", "karate", "Roman-empire", "Amazon-ratings", "Minesweeper", "Tolokers"]
@@ -30,7 +31,9 @@ LIFTINGS = {
     "NeighborhoodComplexLifting": NeighborhoodComplexLifting,
     "SimplicialKHopLifting":SimplicialKHopLifting,
     "CellCycleLifting": CellCycleLifting,
+    "DiscreteConfigurationComplexLifting": DiscreteConfigurationComplexLifting,
     "HypergraphKHopLifting": HypergraphKHopLifting
+    
 }
 PATH = "../DATA/DATASETS"
 
