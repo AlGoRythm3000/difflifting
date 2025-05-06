018ff0eb61434e7738948cf0fd28d56e513cc340
Gabriel Duarte <39168030+gjduart@users.noreply.github.com>
2025-05-06 18:19:13 -0300

    Fixing datasets for node classification

--- dataset/dataset_handler.py
+++ dataset/dataset_handler.py
@@ -367,13 +367,14 @@ def get_node_prediction_dataset(dataset, args,dim=None, seed=42):
         # data.edge_index_undirected= remove_duplicated_edges(data.edge_index)
     elif dataset in COAUTHOR_DATASETS:
         dataset = Coauthor(root='data', name=dataset, transform=T.NormalizeFeatures())
+
         if args.gnn == "GPS":
             dataset = add_positional_encoding(args, dataset)
         if args.lifting == "diffLifting":
             data = dataset[0]
         else:
             data = lift_topology(dataset, args)[0]
-
+        data = random_coauthor_amazon_splits(data, dataset.num_classes, None)
 
     elif dataset in HETEROPHILIC_DATASETS:
         if dataset in WEBKBDatasets:
@@ -424,8 +425,42 @@ def add_positional_encoding(args, dataset):
     return dataset
 
 
-
 import torch_geometric
+
+def index_to_mask(index, size):
+    mask = torch.zeros(size, dtype=torch.bool, device=index.device)
+    mask[index] = 1
+    return mask
+
+def random_coauthor_amazon_splits(data, num_classes, lcc_mask):
+    # Set random coauthor/co-purchase splits:
+    # * 20 * num_classes labels for training
+    # * 30 * num_classes labels for validation
+    # rest labels for testing
+
+    indices = []
+    if lcc_mask is not None:
+        for i in range(num_classes):
+            index = (data.y[lcc_mask] == i).nonzero().view(-1)
+            index = index[torch.randperm(index.size(0))]
+            indices.append(index)
+    else:
+        for i in range(num_classes):
+            index = (data.y == i).nonzero().view(-1)
+            index = index[torch.randperm(index.size(0))]
+            indices.append(index)
+
+    train_index = torch.cat([i[:20] for i in indices], dim=0)
+    val_index = torch.cat([i[20:50] for i in indices], dim=0)
+
+    rest_index = torch.cat([i[50:] for i in indices], dim=0)
+    rest_index = rest_index[torch.randperm(rest_index.size(0))]
+
+    data.train_mask = index_to_mask(train_index, size=data.num_nodes)
+    data.val_mask = index_to_mask(val_index, size=data.num_nodes)
+    data.test_mask = index_to_mask(rest_index, size=data.num_nodes)
+
+    return data
 
 
 class DataloadDataset(torch_geometric.data.Dataset):
